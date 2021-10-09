b0f4e7070edf7bd9de2be9007815fad89e97ed02
JakeSantini <90581545+JakeSantini@users.noreply.github.com>
2021-10-09 15:49:45 +1100

    Add files via upload

--- destination/scripts/pick.py
+++ destination/scripts/pick.py
@@ -20,6 +20,7 @@ def head_tilt():
     head_client.send_goal(goal)
     head_client.wait_for_result()
 
+
 # Function that resets the head tilt
 def head_reset():
     head_client = actionlib.SimpleActionClient("head_controller/point_head", PointHeadAction)
@@ -33,6 +34,7 @@ def head_reset():
     head_client.send_goal(goal)
     head_client.wait_for_result()
 
+
 # Function that closes the gripper
 def close_gripper():
     gripper = actionlib.SimpleActionClient("gripper_controller/gripper_action", GripperCommandAction)
@@ -42,6 +44,7 @@ def close_gripper():
     goal.command.position = 0
     gripper.send_goal(goal)
     gripper.wait_for_result()
+
 
 # Function that opens the gripper
 def open_gripper():
@@ -53,8 +56,47 @@ def open_gripper():
     gripper.send_goal(goal)
     gripper.wait_for_result()
 
-# Take the first marker we see and get its information
-def marker_callback(msg):
+
+# Function that tucks the arm into the 'home' position
+def tuck_arm():
+    move_group = MoveGroupInterface("arm_with_torso", "base_link")
+ 
+    joints = ["torso_lift_joint", "shoulder_pan_joint", "shoulder_lift_joint", "upperarm_roll_joint",
+                  "elbow_flex_joint", "forearm_roll_joint", "wrist_flex_joint", "wrist_roll_joint"]
+    pose = [0.05, 1.32, 1.40, -0.2, 1.72, 0.0, 1.66, 0.0]
+
+    # Plans the joints in joint_names to angles in pose
+    move_group.moveToJointPosition(joints, pose, wait=True)
+
+
+# Function that raises the torso when the arm is tucked
+def raise_torso():
+    move_group = MoveGroupInterface("arm_with_torso", "base_link")
+ 
+    joints = ["torso_lift_joint", "shoulder_pan_joint", "shoulder_lift_joint", "upperarm_roll_joint",
+                  "elbow_flex_joint", "forearm_roll_joint", "wrist_flex_joint", "wrist_roll_joint"]
+    pose = [0.4, 1.32, 1.40, -0.2, 1.72, 0.0, 1.66, 0.0]
+
+    # Plans the joints in joint_names to angles in pose
+    move_group.moveToJointPosition(joints, pose, wait=True)
+
+
+# Function that lowers the torso when the arm is tucked
+def lower_torso():
+    move_group = MoveGroupInterface("arm_with_torso", "base_link")
+ 
+    joints = ["torso_lift_joint", "shoulder_pan_joint", "shoulder_lift_joint", "upperarm_roll_joint",
+                  "elbow_flex_joint", "forearm_roll_joint", "wrist_flex_joint", "wrist_roll_joint"]
+    pose = [0.05, 1.32, 1.40, -0.2, 1.72, 0.0, 1.66, 0.0]
+
+    # Plans the joints in joint_names to angles in pose
+    move_group.moveToJointPosition(joints, pose, wait=True)
+
+
+# Function to pickup an object
+def pick_callback(msg):
+#def pick():
+    # Get tf information
     marker = msg.transforms[0]
     ID = marker.fiducial_id   
     trans = marker.transform.translation
@@ -62,29 +104,6 @@ def marker_callback(msg):
     #print 'Fiducial', ID
     #print 'Translation: ', trans.x, trans.y, trans.z
     #print 'Rotation: ', rot.x, rot.y, rot.z, rot.w
-
-    
-    
-
-# Subscribe to aruco_detect topics for marker to camera transforms
-def marker_listener():
-    rospy.Subscriber("fiducial_transforms", FiducialTransformArray, marker_callback)
-    rospy.spin()    
-
-
-
-
-
-
-if __name__ == '__main__':
-    rospy.init_node("pick")
-    open_gripper()
-    head_tilt()
-    marker_listener()
-# pick up object + move to arm home position
-    head_reset()
-#navigation
-#place
 
     # Create move group interface for Fetch
     move_group = MoveGroupInterface("arm_with_torso", "base_link")
@@ -100,10 +119,6 @@ if __name__ == '__main__':
     planning_scene.addCube("my_left_ground", 2, 0.0, 1.2, -1.0)
     planning_scene.addCube("my_right_ground", 2, 0.0, -1.2, -1.0)
 
-    # This is the wrist link not the gripper itself
-    #gripper_frame = 'wrist_roll_link'
-    gripper_frame = 'gripper_link'
-
     # Position and rotation of poses
     #gripper_poses = [Pose(Point(trans.x, trans.y, trans.z + 0.1),Quaternion(rot.x, rot.y, rot.z, rot.w)),pose(Point(trans.x, trans.y, trans.z),Quaternion(rot.x, rot.y, rot.z, rot.w))]
     gripper_poses = [Pose(Point(0.5, 0.5, 1),Quaternion(0,0,0,0.1)),Pose(Point(0.5, 0.5, 1.1),Quaternion(0,0,0,0.1))]
@@ -117,21 +132,34 @@ if __name__ == '__main__':
         gripper_pose_stamped.pose = pose
 
         # Move gripper frame to the pose specified
-        move_group.moveToPose(gripper_pose_stamped, gripper_frame)
-        result = move_group.get_move_action().get_result()
+        move_group.moveToPose(gripper_pose_stamped, 'gripper_link')
 
-        if result:
-	    # Checking the MoveItErrorCode
-	    if result.error_code.val == MoveItErrorCodes.SUCCESS:
-	        rospy.loginfo("Success")
-	    else:
-	    # If you get to this point please search for:
-	    # moveit_msgs/MoveItErrorCodes.msg
-	        rospy.logerr("Arm goal in state: %s",
-	                     move_group.get_move_action().get_state())
-        else:
-	    rospy.logerr("MoveIt! failure no result returned.")
+    close_gripper()
+    tuck_arm()
+    lower_torso()
+    head_reset()
 
-        # This stops all arm movement goals
-        # It should be called when a program is exiting so movement stops
-        move_group.get_move_action().cancel_all_goals()
+
+# Subscribe to aruco_detect topics for marker to camera transforms
+def pick_listener():
+    raise_torso()
+    head_tilt()
+    open_gripper()
+    rospy.Subscriber("fiducial_transforms", FiducialTransformArray, pick_callback)
+    rospy.spin()    
+
+
+if __name__ == '__main__':
+    rospy.init_node("pick")
+    pick_listener()
+    #raise_torso()
+    #head_tilt()
+    #open_gripper()
+    #pick()
+
+
+
+
+
+
+
